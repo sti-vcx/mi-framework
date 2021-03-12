68c835799a6d45871916cefee63709a86e1a4d30
Ken Chapple <ken.chapple@gmail.com>
2021-03-12 10:16:10 -0500

    Added abstract entity for models that are DB models

---
+++ src/Mi2/Framework/AbstractEntity.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace Mi2\Framework;
+
+use OpenEMR\Events\BoundFilter;
+
+class AbstractEntity extends AbstractModel
+{
+    public static $table = null;
+
+    /**
+     * Given a record set, get the next row, converted to
+     * a model
+
+     * @param $record_set
+     */
+    public static function getNext($record_set, $class)
+    {
+        $row = sqlFetchArray($record_set);
+        $model = new $class($row);
+        return $model;
+    }
+
+    public function getData() {
+        $data = $this->toArray();
+        unset($data['members']);
+        return $data;
+    }
+
+    public function save() {
+        $result = null;
+        if (property_exists($this, 'id')) {
+            $result = self::update($this->id, $this->getData());
+        } else {
+            $result = self::create($this->getData());
+        }
+
+        return $result;
+    }
+
+    public static function find( $id )
+    {
+        $table = static::$table;
+        $statement = "SELECT *
+                FROM $table
+                WHERE id = ?";
+        $result = sqlStatement( $statement, array( $id ) );
+        $referral = false;
+        if ( $result ) {
+            $referral = sqlFetchArray( $result );
+        }
+
+        return $referral;
+    }
+
+    /**
+     * @param BoundFilter|null $filter
+     * @return false|\recordset
+     *
+     * The result of this function can be used to iterate over the recordset
+     * using sqlFetchArray()
+     */
+    public static function all(BoundFilter $filter = null)
+    {
+        $table = static::$table;
+        $statement = "SELECT *
+                FROM $table";
+        if ($filter) {
+            $statement .= " " . $filter->getFilterClause();
+            if (count($filter->getBoundValues())) {
+                $result = sqlStatement($statement, $filter->getBoundValues());
+            } else {
+                $result = sqlStatement($statement);
+            }
+        } else {
+            $result = sqlStatement($statement);
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param array $fields
+     * @return int (last insert ID)
+     */
+    public static function create(array $fields)
+    {
+        $table = static::$table;
+
+        // Build SQL to insert new encounter
+        $count = 0;
+        $sql = "INSERT INTO $table SET ";
+        foreach ($fields as $key => $value) {
+            $sql .= "$key = ?";
+            if ( $count < count( $fields ) - 1 ) {
+                $sql .= ", ";
+            }
+            $count++;
+        }
+
+        return sqlInsert( $sql, $fields );
+    }
+
+    public static function update($id, array $fields)
+    {
+        $table = static::$table;
+        $count = 0;
+        $insert_fields = [];
+        $sql = "UPDATE $table SET ";
+        foreach ( $fields as $key => $value ) {
+            $insert_fields [] = $value;
+            $sql .= "$key = ?";
+            if ( $count < count( $fields ) - 1 ) {
+                $sql .= ", ";
+            }
+            $count++;
+        }
+
+        $sql .= " WHERE id = ?";
+        $insert_fields[] = $id;
+
+        return sqlStatement($sql, $insert_fields);
+    }
+
+    public static function delete($id)
+    {
+        $table = static::$table;
+        $sql = "DELETE FROM $table WHERE id = ?";
+        $result = sqlStatement($sql, [$id]);
+    }
+}
